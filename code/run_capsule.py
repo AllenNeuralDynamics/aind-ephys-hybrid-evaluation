7b217dcb2b254a5f9e5c63438ddb7cddd8b4015b
Alessio Buccino <alejoe9187@gmail.com>
2025-06-12 14:35:31 +0200

    use SLURM_CPUS_ON_NODE instead of SLURM_JOB_CPUS_PER_NODE

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -333,8 +333,8 @@ def compute_additional_metrics(study, metric_names):
 
 
 if __name__ == "__main__":
-    # Use CO_CPUS/SLURM_JOB_CPUS_PER_NODE env variable if available
-    N_JOBS_EXT = os.getenv("CO_CPUS") or os.getenv("SLURM_JOB_CPUS_PER_NODE")
+    # Use CO_CPUS/SLURM_CPUS_ON_NODE env variable if available
+    N_JOBS_EXT = os.getenv("CO_CPUS") or os.getenv("SLURM_CPUS_ON_NODE")
     N_JOBS = int(N_JOBS_EXT) if N_JOBS_EXT is not None else -1
     si.set_global_job_kwargs(n_jobs=N_JOBS, progress_bar=False)
 
