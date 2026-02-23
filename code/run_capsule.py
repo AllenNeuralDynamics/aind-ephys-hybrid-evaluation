50b88d37e93f27b092a3abf75eaad5462286c921
Alessio Buccino <alejoe9187@gmail.com>
2026-02-23 11:08:12 +0100

    Use N_JOBS_EXT

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -373,8 +373,8 @@ def compute_additional_metrics(study, metric_names):
 
 
 if __name__ == "__main__":
-    # Use CO_CPUS/SLURM_CPUS_ON_NODE env variable if available
-    N_JOBS_EXT = os.getenv("CO_CPUS") or os.getenv("SLURM_CPUS_ON_NODE")
+    # Use CO_CPUS/N_JOBS_EXT env variable if available
+    N_JOBS_EXT = os.getenv("CO_CPUS") or os.getenv("N_JOBS_EXT")
     N_JOBS = int(N_JOBS_EXT) if N_JOBS_EXT is not None else -1
     si.set_global_job_kwargs(n_jobs=N_JOBS, progress_bar=False)
 
